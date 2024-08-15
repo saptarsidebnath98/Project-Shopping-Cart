588b7e7833beb066fc1aa61527166cdb0fc7b76e
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-08-15 22:58:40 +0530

    creating shimmer effect design in process

--- src/components/shimmer effect/ShimmerProductInfo.jsx
+++ src/components/shimmer effect/ShimmerProductInfo.jsx
@@ -1,10 +1,17 @@
 
 const ShimmerProductInfo = () => {
+    const shimProductArr = new Array(7).fill("");
   return (
     <div className="container shimmer-productInfo">
         <div className="shimmer-product-mainDiv">
             <div className="shimmer-product-upper">upper</div>
-            <div className="shimmer-product-lower">lower</div>
+            <div className="shimmer-product-lower">
+                <ul className="shimmer-productInfo-list">
+                    {shimProductArr.map((item, i)=> 
+                        <li className="shimmer-productInfo-items" key={i}></li>
+                    )}
+                </ul>
+            </div>
         </div> 
     </div>
   )
